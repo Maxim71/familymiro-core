eb7ccd55e1bb80f78f8b805c05d718cedb2213e0
CaptainMaxim <max@miroha.ru>
2026-09-16 09:35:07 +0300

    Miroha Core 1.8 - Полное разделение слоев архитектуры: HTML отдельно, CSS отдельно, JS отдельно

---
+++ storage_control/static/storage_control/js/pto_logic.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("🛰️ [Miroha ИТР]: Модуль pto_logic.js успешно инициализирован в ОЗУ.");
+});
+
+function triggerPtoSync() {
+    alert("🦔 Робот-Ёжик: Сводный ИТР-отчет М-19 и КС-2 выгружен в ОЗУ бухгалтерии из изолированного JS-модуля!");
+}
