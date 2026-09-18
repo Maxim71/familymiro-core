0eb2bb2c00b7f59e3194a180df85a6589025a2cf
CaptainMaxim <max@miroha.ru>
2026-09-18 10:56:23 +0300

    Miroha Core 10.0 - Платежный шлюз СБП Альфа-Банка полностью интегрирован с расчетом 2% маржи

--- storage_control/urls.py
+++ storage_control/urls.py
@@ -5,7 +5,6 @@ urlpatterns = [
     path('', views.index_vancouver, name='index_vancouver'),
     path('pto/<int:act_id>/', views.pto_cabinet, name='pto_cabinet'),
     path('pto/<int:act_id>/exit/', views.pto_cabinet, name='pto_cabinet_exit'),
-    path('capsule/', views.capsule_time_vault, name='capsule_time_vault'),
     path('radar/', views.neuro_radar_dashboard, name='neuro_radar_dashboard'),
     path('api/send-stream/', views.send_to_stream_api, name='send_to_stream_api'),
     path('api/live-stream-data/', views.live_stream_dashboard_api, name='live_stream_dashboard_api'),
@@ -13,4 +12,5 @@ urlpatterns = [
     path('api/ezhik-voice-notepad/', views.ezhik_voice_notepad_api, name='ezhik_voice_notepad_api'),
     path('api/neuro-radar-voice/', views.neuro_radar_voice_api, name='neuro_radar_voice_api'),
     path('api/computer-vision-m19/', views.computer_vision_m19_api, name='computer_vision_m19_api'),
+    path('api/checkout-sbp-payment/', views.checkout_sbp_payment_api, name='checkout_sbp_payment_api'),
 ]
