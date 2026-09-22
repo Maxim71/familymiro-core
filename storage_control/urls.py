52ca46175e9a75024463b6466609f3f85c4915a3
CaptainMaxim <max@miroha.ru>
2026-09-22 09:52:58 +0300

    Miroha Core 37.0 - Полная ликвидация 404 ошибки на Радаре, внедрен 3D-параллакс, карта ТСФ и скрытый футер авторских прав без пафоса

--- storage_control/urls.py
+++ storage_control/urls.py
@@ -5,8 +5,7 @@ urlpatterns = [
     path('', views.index_vancouver, name='index_vancouver'),
     path('pto/<int:act_id>/', views.pto_cabinet, name='pto_cabinet'),
     path('radar/', views.neuro_radar_dashboard, name='neuro_radar_dashboard'),
-    
-    # 📱 ДИНАМИЧЕСКИЙ ТЕНДЕРНЫЙ ШЛЮЗ ПРОРАБА IPROPAB (ТВОИ 27 ПУТЕЙ!)
+    path('api/metabase-xray-heatmap/', views.metabase_xray_heatmap_api, name='metabase_xray_heatmap_api'),
     path('ipropab/<str:company>/<str:name>/<str:task_id>/status/', views.ipropab_agent_cabinet, name='ipropab_agent_cabinet'),
     path('ipropab/<str:company>/<str:name>/<str:task_id>/status/submit/', views.ipropab_submit_photo_api, name='ipropab_submit_photo_api'),
 ]
